e13fb8eef7f9876db1b892118303e505db4c5bba
SachiMehta <8547038-SachiMehta@users.noreply.replit.com>
2023-08-23 02:38:20 +0000

    move den code to script js

--- static/script.js
+++ static/script.js
@@ -1,3 +1,35 @@
+function hover2(page, element) {
+  // if page reference is nav access the nav folder 
+    img_ID2.setAttribute('src', "/static/img/"+ page +"/nav" + element + ".png");
+  }
+  
+  function unhover2(page) {
+    // if the page reference is nav accesss the nav folder
+    img_ID2.setAttribute('src', "/static/img/"+ page);
+  }
+
+  function sub(mood = "ignore", updat="ignore") {
+    if (mood != "ignore")
+    {document.getElementById('moodInput').value = mood;
+    document.getElementById("moodsub").click();}
+    else if (updat!= "ignore") 
+    {document.getElementById('updateViewHid').value = updat;
+      document.getElementById("updateView").click();}
+    
+  }
+
+window.addEventListener('beforeunload', pageClosed);
+
+function pageClosed()
+{
+  document.getElementById('updateViewHid').value = "close";
+  document.getElementById("updateView").click();
+  window.onbeforeunload = null;
+}
+
+
+
+
 window.onload = function() {
   var ImageMap = function(map, img) {
     var n,
